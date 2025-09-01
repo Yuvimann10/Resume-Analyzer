e8b96c3bb1fbd1ac88f8c237e556df9a35770af0
YUVRAJ MANN <yuvrajmann@YUVRAJs-MacBook-Air-2.local>
2025-09-01 11:04:55 -0400

    hi

---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from sqlmodel import SQLModel, Field 
+from typing import Optional 
+from datetime import datetime 
+
+class User(SQLModel,table=True):
+    id: Optional[int] = Field(default = None, primary_key = True)
+    username: Optional[str] 
+    email: Optional[str]
+    created_at: datetime = Field(default_factory = datetime.utcnow)
+
+class Resume(SQLModel,table=True):
+    id: Optional[int] = Field(default = None, primary_key = True)
+    user_id: int
+    original_text: str 
+    improved_text: Optional[str] = None 
+    created_at: datetime = Field(default_factory = datetime.utcnow)
+
+
+
+
+    
